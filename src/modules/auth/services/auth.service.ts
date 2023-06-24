178d79384cc12f41560d7d095b0f8cdd89276c8d
ZachLee12 <leezhengyang22@gmail.com>
2023-06-24 18:45:37 +0200

    Compare encrypted passwords

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,18 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt/dist';
 import { UsersService } from '../../users/services/users.service';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
     constructor(
         private jwtService: JwtService,
         private usersService: UsersService
-    ) {
-    }
+    ) { }
 
-    async signIn(username: string, pass: string) {
+    async signIn(username: string, password: string) {
         const user = await this.usersService.getOneUser(username)
-        if (user?.password !== pass) {
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) {
             throw new UnauthorizedException('Incorrect credentials')
         }
         const payload = { sub: user.id, username: user.username, age: user.age };
