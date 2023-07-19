ec32483ebfb5773ada9ef23207870f4e8397c979
ZachLee12 <leezhengyang22@gmail.com>
2023-07-19 13:50:05 +0200

    Add movie in separate js file

--- movies.js
+++ movies.js
@@ -1,10 +1,73 @@
-const logan = {
-    name: 'Logan',
-    description: 'In a future where mutants are nearly extinct, an elderly and weary Logan leads a quiet life. But when Laura, a mutant child pursued by scientists, comes to him for help, he must get her to safety.',
-    actors: ['Ryan Gosling', 'Hugh Jackman', 'Ryan Reynolds'],
-    genres: ['Action', 'Thriller'],
-    showtimes: ['08:00 AM', '09:00 AM', '12:00 PM', '13:00 PM', '15:00 PM', '20:00 PM'],
-    imgUrl: "https://drive.google.com/uc?id=1AtKybsFE_nyCIWInWmfiVNVW66haWIQR&export=download"
-}
+const movies = [
+    {
+        name: 'Logan',
+        description: 'In a future where mutants are nearly extinct, an elderly and weary Logan leads a quiet life. But when Laura, a mutant child pursued by scientists, comes to him for help, he must get her to safety.',
+        actors: ['Ryan Gosling', 'Hugh Jackman', 'Ryan Reynolds'],
+        genres: ['Action', 'Thriller'],
+        showtimes: ['08:00 AM', '09:00 AM', '12:00 PM', '13:00 PM', '15:00 PM', '20:00 PM'],
+        imgUrlVertical: "https://drive.google.com/uc?id=1AtKybsFE_nyCIWInWmfiVNVW66haWIQR&export=download",
+        imgUrlHorizontal: "https://drive.google.com/uc?id=14CY1Pq_o1YViG6Thu15B03bMQHY6eneq&export=download"
+    },
+    {
+        name: 'Elemental',
+        description: 'The film journeys alongside an unlikely pair, Ember and Wade, in a city where fire-, water-, land- and air-residents live together. The fiery young woman and the go-with-the-flow guy are about to discover something elemental: how much they actually have in common.',
+        actors: ['Leah Lewis', 'Mamoudou Athie', 'Shila Ommi'],
+        genres: ['Family', 'Romance', 'Comedy'],
+        showtimes: ['08:00 AM', '09:00 AM', '13:00 PM', '20:00 PM'],
+        imgUrlVertical: "https://drive.google.com/uc?id=17-37HD0cO0QefIfTptfW-fIue--qFgbT&export=download",
+        imgUrlHorizontal: "https://drive.google.com/uc?id=1-eDMG5js0ldwRIbxB_1eFvkZA27-mexq&export=download"
+    },
+    {
+        name: 'Coco',
+        description: `Despite his family's baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events.`,
+        actors: ['Anthony Gonzalez', 'Benjamin Bratt'],
+        genres: ['Family', 'Comedy'],
+        showtimes: ['08:00 AM', '09:00 AM', '10:00 AM', '12:00 PM', '13:00 PM', '15:00 PM', '20:00 PM', '23:00 PM'],
+        imgUrlVertical: "https://drive.google.com/uc?id=1HSxcr_60SX9ciUc-83cmVOxw8YMb8-sE&export=download",
+        imgUrlHorizontal: "https://drive.google.com/uc?id=1GqpfgSC_hvCh0CHP_6y8IRFUZl6Vx5Ee&export=download"
+    },
+    {
+        name: 'Blade Runner 2049',
+        description: `Thirty years after the events of Blade Runner (1982), a new Blade Runner, L.A.P.D. Officer "K" (Ryan Gosling), unearths a long-buried secret that has the potential to plunge what's left of society into chaos.`,
+        actors: ['Ryan Gosling', 'Harrison Ford', 'Dave Bautista', 'Ana de Armas'],
+        genres: ['Action', 'Thriller', 'Adventure'],
+        showtimes: ['09:00 AM', '10:00 AM', '12:30 PM', '13:00 PM', '15:00 PM', '20:00 PM', '21:30 PM'],
+        imgUrlVertical: "https://drive.google.com/uc?id=1E8USBmO7o00sgsdYWmczEzLbyGEPKIfo&export=download",
+        imgUrlHorizontal: "https://drive.google.com/uc?id=1zFyf58sGE-bXS28pmLpzy-cc7Bd5nIz6&export=download"
+    },
+    {
+        name: 'Logan',
+        description: `In 2029 the mutant population has shrunk significantly due to genetically modified plants designed to reduce mutant powers and the X-Men have disbanded. Logan, whose power to self-heal is dwindling, has surrendered himself to alcohol and now earns a living as a chauffeur.`,
+        actors: ['Hugh Jackman', 'Patrick Stewart', 'Dafne Keen'],
+        genres: ['Action', 'Adventure', 'Thriller'],
+        showtimes: ['09:30 AM', '10:30 AM', '15:00 PM', '20:00 PM', '21:30 PM'],
+        imgUrlVertical: "https://drive.google.com/uc?id=1AtKybsFE_nyCIWInWmfiVNVW66haWIQR&export=download",
+        imgUrlHorizontal: "https://drive.google.com/uc?id=14CY1Pq_o1YViG6Thu15B03bMQHY6eneq&export=download"
+    },
+    {
+        name: 'Gifted',
+        description: `Frank Adler (Chris Evans) is a single man raising a child prodigy - his spirited young niece Mary (Mckenna Grace) in a coastal town in Florida. Frank's plans for a normal school life for Mary are foiled when the seven-year-old's mathematical abilities come to the attention of Frank's formidable mother Evelyn (Lindsay Duncan) whose plans for her granddaughter threaten to separate Frank and Mary.`,
+        actors: ['Chris Evans', 'Mckenna Grace', 'Lindsay Duncan'],
+        genres: ['Drama', 'Family'],
+        showtimes: ['08:30 AM', '10:30 AM', '15:30 PM', '16:00 PM', '20:00 PM'],
+        imgUrlVertical: "https://drive.google.com/uc?id=1IGwCUNqbHMxe-iwMfS7vbs1ma_Scv1e3&export=download",
+        imgUrlHorizontal: "https://drive.google.com/uc?id=1E0Ue8xCF6T6IoqTmYSAskP2M9PkBMlZz&export=download"
+    },
+    {
+        name: 'Leave No Trace',
+        description: `Will (Ben Foster) and his teenage daughter, Tom (Thomasin Harcourt McKenzie), have lived off the grid for years in the forests of Portland, Oregon. When their idyllic life is shattered, both are put into social services. After clashing with their new surroundings, Will and Tom set off on a harrowing journey back to their wild homeland.`,
+        actors: ['Ben Foster', 'Thomasin Harcourt McKenzie', 'Jeffery Rifflard'],
+        genres: ['Drama', 'Adventure'],
+        showtimes: ['08:30 AM', '10:30 AM', '11:40 AM', '14:00 PM', '15:30 PM', '16:00 PM', '20:00 PM'],
+        imgUrlVertical: "https://drive.google.com/uc?id=1vUpQBSjUTS2fdAdnVLfLjPhgzSe1_XQW&export=download",
+        imgUrlHorizontal: "https://drive.google.com/uc?id=17nGIK2QFXgl_jvtyS5Joo89trOC0hHdA&export=download"
+    },
+
+
+
+
+
+
+]
 
 // const
